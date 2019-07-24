76db5aa0666fafb892cdcd944a4aba3c446906bc
Darren Cook <thelurkingdev@gmail.com>
2019-07-23 22:23:39 -0400

    Created PlayerMovement and CharacterMovement scripts.

---
+++ Assets/PlayerMovement/CharacterMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Movement2D
+{
+    public class CharacterMovement : MonoBehaviour
+    {
+        private Transform _transform;
+        private float _speed = 50f;
+
+        private void Start()
+        {
+            _transform = this.gameObject.transform;
+        }
+        protected virtual Vector2 Move(Vector2 movementVector)
+        {
+            _transform.position += new Vector3(movementVector.x, movementVector.y, _transform.position.z) * _speed * Time.deltaTime;
+            return _transform.position;
+        }        
+    }
+}
---
+++ Assets/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Movement2D
+{
+    public class PlayerMovement : CharacterMovement
+    {
+        private void Update()
+        {
+            var movementVector = GetMovementDirectionFromKeyboardInput();
+            if(movementVector != Vector2.zero)
+            {
+                base.Move(movementVector);
+            }
+        }
+
+        private Vector2 GetMovementDirectionFromKeyboardInput()
+        {
+            float moveX = 0f;
+            float moveY = 0f;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveY = +1f;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                moveY = -1f;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveX = -1f;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                moveX = +1f;
+            }
+
+            return new Vector2(moveX, moveY).normalized;
+        }
+    }
+}
