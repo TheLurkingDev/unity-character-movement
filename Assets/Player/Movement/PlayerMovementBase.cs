f6f5536ee1389f861f62a38a3ce5162bfdd45f2a
Darren Cook <thelurkingdev@gmail.com>
2019-07-30 19:59:37 -0400

    Added PlayerMovementBase

---
+++ Assets/Player/Movement/PlayerMovementBase.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace MovementVector2D
+{
+    public abstract class PlayerMovementBase : MonoBehaviour
+    {
+        private Transform _transform;
+        private float _speed = 5f;
+        private Animator _animator;
+        private Vector2 _lastMovementDirection;
+        private PlayerAnimation _playerAnimation;
+        [SerializeField] private PlayerAnimationScriptableObject _playerAnimationClips;
+
+        protected void BaseStart()
+        {
+            _transform = this.gameObject.transform;
+            _animator = GetComponent<Animator>();
+            _lastMovementDirection = Vector2.down;
+            _playerAnimation = new PlayerAnimation(_animator, _playerAnimationClips);
+        }
+
+        protected abstract Vector2 Move(Vector2 movementVector);
+
+        protected abstract Vector2 GetMovementDirectionFromKeyboardInput();
+
+        protected void PlayWalkAnimation(Vector2 movementVector)
+        {
+            _playerAnimation.PlayAnimation(AnimationType.Walk, movementVector);
+            _lastMovementDirection = movementVector;
+        }
+
+        protected void PlayIdleAnimation()
+        {
+            _playerAnimation.PlayAnimation(AnimationType.Idle, _lastMovementDirection);
+        }
+    }
+}
