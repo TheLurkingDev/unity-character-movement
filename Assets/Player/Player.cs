a5e158c2c19036fa733f16b983556eef9d7a8df4
Darren Cook <thelurkingdev@gmail.com>
2019-08-04 12:41:53 -0400

    Added Player class and began setup for peristing data.

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Persistence;
+
+namespace Player
+{
+    public class Player : MonoBehaviour
+    {
+        private Transform _transform;
+
+        private void Start()
+        {
+            _transform = GetComponent<Transform>();
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+
+            }
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+
+            }
+        }
+    }
+}
