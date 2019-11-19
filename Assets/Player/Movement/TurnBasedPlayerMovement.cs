af5d4a20e18787f7eb2df6fda24347dd9baf9a66
Darren Cook <thelurkingdev@gmail.com>
2019-11-19 00:42:38 -0500

    Refactored Move() method for turn-based player movement to use movementVector parameter.

--- Assets/Player/Movement/TurnBasedPlayerMovement.cs
+++ Assets/Player/Movement/TurnBasedPlayerMovement.cs
@@ -26,22 +26,7 @@ namespace TheLurkingDev.Player.Movement2D
 
         protected override Vector2 Move(Vector2 movementVector)
         {
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-            {
-                _transform.Translate(Vector3.up);
-            }
-            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-            {
-                _transform.Translate(Vector3.down);
-            }
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            {
-                _transform.Translate(Vector3.left);
-            }
-            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            {
-                _transform.Translate(Vector3.right);
-            }
+            _transform.Translate(movementVector);            
 
             return _transform.position;
         }
